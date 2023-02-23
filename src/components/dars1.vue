a7b55970a77326d8b8bc5cb27652f6b37dc93f98
@khan13 <aabdulaxadkhan@gmail.com>
2023-02-23 18:07:45 +0500

    vue Reactive  Ref

---
+++ src/components/dars1.vue
@@ -0,0 +1,76 @@
+<script setup>
+// import { reactive } from 'vue'
+
+// const state = reactive({ count: 0 })
+
+// function increment() {
+//   state.count++
+// }
+/*****************************************************************/ 
+
+
+// // chuqur Reactive 
+// import { reactive } from 'vue'
+// const obj = reactive({
+//   nested: { count: 0 },
+//   arr: ['foo', 'bar']
+// })
+// function mutateDeeply() {
+//   obj.nested.count++
+//   obj.arr.push('baz')
+// }
+/*****************************************************************/ 
+
+// import { reactive } from 'vue'
+// const state = reactive({ count: 0 })
+// function increment() {
+//   state.count++
+// }
+// let n = state.count
+// n--
+
+/*****************************************************************/ 
+
+
+import { ref } from 'vue'
+const state = ref(0)
+function increment() {
+  state.value++
+}
+let n = state.value
+n++
+</script>
+
+<template>
+  <div>
+    <!-- <div>
+      count {{ state.count }}
+    </div>
+    <button class="btn btn-warning" @click="increment">
+      add
+    </button> -->
+  <!-- /*****************************************************************/ -->
+
+    <!-- {{ obj.nested.count }}
+    {{ obj.arr.join(' ') }}
+    <button class="btn btn-warning" @click="mutateDeeply">
+      add
+    </button> -->
+  <!-- /*****************************************************************/ -->
+
+      <!-- count {{ state.count }}
+      {{ n }}
+      <button class="btn btn-warning" @click="increment">
+        add
+      </button> -->
+
+  <!-- /*****************************************************************/ -->
+  
+      Ref {{ state }}
+      {{ n }}
+      <button class="btn btn-warning" @click="increment">
+        add
+      </button>
+    </div>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Dars1 from '../components/dars1.vue'
+</script>
+
+<template>
+  <main>
+    <Dars1 />
+  </main>
+</template>
